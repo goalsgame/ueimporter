a1cadfbf5254a6d8805988495bd433828c2bf33d
Andreas Andersson <andreas@neoboid.com>
2022-03-29 16:12:20 +0200

    Use Major.Minor.Patch version scheme

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='ueimporter',
-      version='0.1',
+      version='0.1.0',
       description='Imports Unreal Engine releases into plastic repository',
       url='http://github.com/goalsgame/tools-game-utils/ueimporter',
       author='Andreas Andersson',
